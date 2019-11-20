c413f9bdc80d0224def020554d2394216ee134b8
Andrew Allard (RIT Student) <aa9720@ad.rit.edu>
2019-11-20 12:10:49 -0500

    updates

--- RExMotors/src/main.cpp
+++ RExMotors/src/main.cpp
@@ -1,41 +1,49 @@
 #include <Arduino.h>
 #include "../lib/HBridge/HBridge.h"
 
+// The many... many... pin declarations.
 #define frontMotorEncoderA 0
 #define frontMotorEncoderB 1
 #define frontHBridgeA 2
 #define frontHBridgeB 3
 #define frontHBridgeEnable 4
 
-#define backMotorEncoderA 0
-#define backMotorEncoderB 1
-#define backHBridgeA 2
-#define backHBridgeB 3
-#define backHBridgeEnable 4
+#define backMotorEncoderA 5
+#define backMotorEncoderB 6
+#define backHBridgeA 7
+#define backHBridgeB 8
+#define backHBridgeEnable 9
 
-#define leftMotorEncoderA 0
-#define leftMotorEncoderB 1
-#define leftHBridgeA 2
-#define leftHBridgeB 3
-#define leftHBridgeEnable 4
+#define leftMotorEncoderA 10
+#define leftMotorEncoderB 11
+#define leftHBridgeA 12
+#define leftHBridgeB 13
+#define leftHBridgeEnable 14
 
-#define rightMotorEncoderA 0
-#define rightMotorEncoderB 1
-#define rightHBridgeA 2
-#define rightHBridgeB 3
-#define rightHBridgeEnable 4
+#define rightMotorEncoderA 16
+#define rightMotorEncoderB 17
+#define rightHBridgeA 18
+#define rightHBridgeB 19
+#define rightHBridgeEnable 20
+
+#define leftIRPin 21
+#define rightIRPin 22
 
 
+//IR vars
+const int irThreshold = 1100, maxIRReadTime = 2000;
+
+// motors
 L298n frontMotor(frontHBridgeA, frontHBridgeB, frontHBridgeEnable);
 L298n backMotor(backHBridgeA, backHBridgeB, backHBridgeEnable);
 L298n leftMotor(leftHBridgeA, leftHBridgeB, leftHBridgeEnable);
 L298n rightMotor(rightHBridgeA, rightHBridgeB, rightHBridgeEnable);
 
-//Serial crap
+//Serial and communication crap
 bool fullOutput = false;
 String inputString = "";
 float vel = 0, theta = 0, phi = 0;
-
+bool followLineMode = true;
 
 
 void setup() {
@@ -48,6 +56,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(followLineMode){
+    followLine();
+  }
 }
 
 void serialEvent(){
@@ -106,4 +117,91 @@ void parseCMD(){
 
 String returnVelData(){
   return String(vel) + "," + String(theta) +  "," + String(phi);
+}
+
+/*
+For line following things are going to be a bit simplified
+We will only use the left and right wheels
+*/
+
+void followLine(){
+  stopMotors();
+  //Find line with middle reflective sensor
+  //If there is no line with the middle sensor this will exit.
+  int maxCounts=500;
+  //drive forward.
+  forward(90);
+  int timeCounter=0;
+  while(1){
+    if(!followLineMode){
+      stopMotors();
+      return;
+    }
+    while(leftIROnLine()){
+      leftMotor.stop();
+      delay(10);
+      timeCounter++;
+      if(timeCounter>maxCounts){
+        followLineMode=false;
+        return;
+      }
+    }
+    timeCounter=0;
+    leftMotor.start();
+    while(rightIROnLine()){
+      rightMotor.stop();
+      delay(10);
+      timeCounter++;
+      if(timeCounter>maxCounts){
+        followLineMode=false;
+        return;
+      }
+    }
+    timeCounter=0;
+    rightMotor.start();
+  }
+}
+
+void forward(int speed){
+  leftMotor.setDirection(false);
+  rightMotor.setDirection(true);
+  leftMotor.setSpeed(speed);
+  rightMotor.setSpeed(speed);
+  leftMotor.start();
+  rightMotor.start();
+}
+void stopMotors(){
+  frontMotor.stop();
+  backMotor.stop();
+  leftMotor.stop();
+  rightMotor.stop();
+}
+
+bool leftIROnLine(){
+  bool onLine=false;
+  pinMode(leftIRPin, OUTPUT);
+  digitalWriteFast(leftIRPin, HIGH);
+  delayMicroseconds(10);
+  pinMode(leftIRPin, INPUT);
+  long readTime = micros();
+  while(digitalReadFast(leftIRPin) == HIGH && micros() - readTime < maxIRReadTime);
+  int time = micros() - readTime;
+  if(time > irThreshold){
+    onLine = true;
+  }
+  return onLine;
+}
+bool rightIROnLine(){
+  bool onLine=false;
+  pinMode(rightIRPin, OUTPUT);
+  digitalWriteFast(rightIRPin, HIGH);
+  delayMicroseconds(10);
+  pinMode(rightIRPin, INPUT);
+  long readTime = micros();
+  while(digitalReadFast(rightIRPin) == HIGH && micros() - readTime < maxIRReadTime);
+  int time = micros() - readTime;
+  if(time > irThreshold){
+    onLine = true;
+  }
+  return onLine;
 }
